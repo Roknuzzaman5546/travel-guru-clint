7ecd16727598246b5fb2794c97ca427e48de0fae
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-01-02 02:23:13 +0600

    first commit

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,61 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './styles.css'
+import { Pagination } from 'swiper/modules';
+
+import swip1 from '../../assets/images/Sajek.png'
+import swip2 from '../../assets/images/Sreemongol.png'
+import swip3 from '../../assets/images/sundorbon.png'
+
+const Banner = () => {
+    return (
+        <div className='bgimg'>
+            <div className=" flex lg:flex-row flex-col py-28 backdrop-brightness-50">
+                <div className=" w-1/2 ml-10">
+                    <h2 className=" brightness-100 text-6xl font-bold text-white font-sans">Coxs bazar</h2>
+                    <button className=" btn btn-warning mt-40">Booking</button>
+                </div>
+                <div className=' w-1/2'>
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img src={swip1} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sajek</h2>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={swip2} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sremongal</h2>
+                            <div className=' mt-10'></div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={swip3} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sundarbon</h2>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={swip1} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sajek</h2>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={swip2} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sremongal</h2>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={swip3} alt="" />
+                            <h2 className=' text-white text-center text-2xl uppercase font-bold -mt-10'>Sundarbon</h2>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+
+    const link = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/destination">Destination</NavLink></li>
+        <li><NavLink to="/blog">Blog</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li className=" btn btn-warning"><NavLink to="/login">Login</NavLink></li>
+    </>
+
+    return (
+        <div>
+            <div className="navbar fixed z-10">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {link}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div className="py-1 bg-white px-2 rounded-md border border-black">
+                    <FaSearch className=" text-black mr-2"></FaSearch>
+                    <input className=" text-black bg-none" placeholder="Search your destination....." type="text" name="" id="" />
+                </div>
+                <div className="navbar-end">
+                    <div className="hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 items-center">
+                            {link}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
