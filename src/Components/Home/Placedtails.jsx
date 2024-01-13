ad4ab181104e8d0817bda56e6855c90e78a7dde7
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-01-13 11:58:17 +0600

    place details added

--- src/Components/Home/Placedtails.jsx
+++ src/Components/Home/Placedtails.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import 'aos/dist/aos.css';
 import Aos from 'aos';
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 const Placedtails = () => {
     useEffect(() => {
@@ -18,16 +20,28 @@ const Placedtails = () => {
         <div className=" w-10/12 mx-auto">
             <div data-aos="zoom-in" className=" mb-10 mt-5">
                 <h1 className="text-4xl border-2 border-b-slate-400 border-t-slate-400 py-5 w-1/2 mx-auto text-center font-Chinzel uppercase font-bold font-rancho">{place.name}</h1>
-                <h1 className=" text-center mb-2 text-yellow-500 text-xl">---This place is details here---</h1>
+                <h1 className=" text-center text-yellow-500 text-xl">---This place is details here---</h1>
             </div>
             <div>
                 <img className=" w-9/12 mx-auto h-96" src={place.img} alt="" />
                 <div>
-                    <div>
+                    <div className=" flex justify-between items-center mt-4">
                         <h2 className=" text-3xl font-bold font-serif">{place.name}</h2>
                         <h2 className=" font-bold text-2xl font-serif  my-2">{place.title}</h2>
                     </div>
-                    <p><span className=" font-bold">Details:</span> {place.details}</p>
+                    <div className=" flex justify-between items-center mt-1">
+                        <h2 className=" text-3xl font-bold font-serif"><span className=" font-bold">Cost:</span> {place.cost} $</h2>
+                        <Rating
+                            style={{ maxWidth: 180 }}
+                            value={place.rating}
+                            readOnly
+                        />
+                    </div>
+                    <p className="font-serif text-xl mt-2"><span className=" text-xl font-bold font-serif">Details:</span> {place.details}</p>
+                    <div className=" flex justify-between items-center mt-2 mb-5">
+                        <Link to='/dashbord/allplace'><button className=" btn btn-warning font-bold font-mono text-xl">View all place</button></Link>
+                        <Link><button className=" btn btn-warning font-bold font-mono text-xl">Book now</button></Link>
+                    </div>
                 </div>
             </div>
         </div>
