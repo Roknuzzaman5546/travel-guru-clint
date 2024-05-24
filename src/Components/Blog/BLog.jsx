4a8db4042f84e36db4f75740c9aaeb4a1dcd3f3a
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-05-24 13:10:09 +0600

    blog card layout done

--- src/Components/Blog/BLog.jsx
+++ src/Components/Blog/BLog.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
+import { Link, NavLink } from "react-router-dom";
 
 const Blog = () => {
     const [blogs, setBlogs] = useState([])
@@ -12,14 +13,27 @@ const Blog = () => {
 
 
     return (
-        <div className=" mt-36 mb-44 text-center" >
-            <h2>{blogs.length}</h2>
-            <div className=" grid md:grid-cols-2 grid-cols-1 gap-10">
-                {
-                    blogs.map(item => <BlogCard key={item._id} item={item} ></BlogCard>)
-                }
+        <div>
+            <div className="blogBgImg">
+                <div className=" bg-[#000000B2]">
+                    <div className=" w-[35%] mx-auto py-24 text-center">
+                        <h2 className="text-6xl font-bold text-[#ff9c1c] font-serif">All Hotel</h2>
+                        <div className="mt-2">
+                            <NavLink to='/blog' className='navAfter relative font-medium text-base text-white mx-3'>Blog</NavLink>
+                            <Link to="/blog" className=" text-[#ff9c1c]">Blog details</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div >
+            <div className=" mt-36 mb-44 text-center lg:w-3/4 w-11/12  mx-auto">
+                <h2>{blogs.length}</h2>
+                <div className=" grid md:grid-cols-2 grid-cols-1 gap-10">
+                    {
+                        blogs.map(item => <BlogCard key={item._id} item={item} ></BlogCard>)
+                    }
+                </div>
+            </div >
+        </div>
     );
 };
 
--- src/Components/Blog/BlogCard.jsx
+++ src/Components/Blog/BlogCard.jsx
@@ -1,7 +1,10 @@
 import { CiClock2 } from "react-icons/ci";
+import { AiOutlineUser } from "react-icons/ai";
+import { FaComment, FaShareAlt } from "react-icons/fa";
 
 const BlogCard = ({ item }) => {
-    const { details, img, title, date } = item;
+    const { details, img, title, date, bloggerInfo } = item || {};
+    const { bloggerImg, bloggerEmail, bloggerName } = bloggerInfo || {};
 
     return (
         <div className="">
@@ -9,9 +12,25 @@ const BlogCard = ({ item }) => {
                 <div className=" p-[30px] relative">
                     <div className=" border-b border-[#e1e1f0] pb-[15px] mb-[15px]">
                         <ul className=" flex flex-wrap -ml-[12px] -mr-[12px]">
-                            <li className=" flex items-center font-medium pl-3 pr-3">
-                                <CiClock2 />
+                            <li className=" flex items-center font-medium pl-3 pr-3 gap-1">
+                                <i className=" text-[#ff9c1c]"><CiClock2 /></i>
                                 <p>{date}</p>
+                            </li>
+                            <li className=" flex items-center font-medium pl-3 pr-3 gap-1">
+                                <i className=" text-[#ff9c1c]"><AiOutlineUser /></i>
+                                <p>{bloggerName}</p>
+                            </li>
+                            <li className=" flex items-center font-medium pl-3 pr-3 gap-1 ml-20">
+                                <li className=" text-[#ff9c1c]"><FaComment /></li>
+                                <p>3</p>
+                            </li>
+                            <li>
+                                <li className=" text-[#ff9c1c]"><FaShareAlt /></li>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
                             </li>
                         </ul>
                     </div>
