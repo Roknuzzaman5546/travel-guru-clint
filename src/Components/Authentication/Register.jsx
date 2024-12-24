2f8c8c7a6eb9b62a6794f1fdd96430213bf78e0f
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-12-24 23:54:10 +0600

    All system code added with btn

--- src/Components/Authentication/Register.jsx
+++ src/Components/Authentication/Register.jsx
@@ -85,7 +85,7 @@ const Register = () => {
                     </form>
                 </div>
             </div>
-            <div className=" w-1/3 mx-auto my-10">
+            <div className=" w-1/3 mx-auto my-7">
                 <div className="divider divider-neutral">OR</div>
             </div>
 
@@ -94,7 +94,7 @@ const Register = () => {
                 <h2 className="font-bold">Log in with facebook</h2>
             </div> */}
 
-            <div className="flex items-center justify-evenly w-1/4 mx-auto border-2 rounded-full my-4 py-2 cursor-pointer mb-28" onClick={handleGoogleLogin}>
+            <div className="flex items-center justify-evenly w-1/4 mx-auto border-2 rounded-full my-4 py-2 cursor-pointer mb-28 btn" onClick={handleGoogleLogin}>
                 <img loading="lazy" className="w-8 h-8" src={google} alt="" />
                 <h2 className="font-bold">Log in with Google</h2>
             </div>
