2cf4c407a6677032e9ee729a83a928d1aac1e905
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-12-27 23:44:00 +0600

    place some more code added

--- src/Components/Dashbord/Userdash/Choicelist.jsx
+++ src/Components/Dashbord/Userdash/Choicelist.jsx
@@ -44,34 +44,41 @@ const Choicelist = () => {
             <div className=" flex justify-between items-center mt-10 mb-7">
                 <h2 className=" text-3xl font-bold font-mono text-[#ff9c1c]">Place, Hotel & Destination</h2>
             </div>
-            <div className=" grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4">
-                {
-                    choice.length > 0 ?
-                        choice.map(item => <div key={item._id}>
-                            <div className="h-64 flex justify-center gap-2 overflow-hidden bg-base-100 shadow-xl rounded-xl mb-5">
-                                <img className="rounded-lg w-[50%] h-64" src={item.img} alt="Album" />
-                                <div className=" w-full flex flex-col justify-center gap-[2px] scroll-my-3">
-                                    <h2 className="text-2xl font-bold font-sans mt-2 col-span-1">{item.name}</h2>
-                                    <p className="font-bold font-mono">{item.title}</p>
-                                    <p className="font-bold font-mono col-span-1">You:{item.userName}</p>
-                                    <p className="font-bold font-mono col-span-1">Email:{item.email}</p>
-                                    <p className="font-bold font-mono">Cost:{item.cost} $</p>
-                                    <div className="flex flex-col md:flex-row justify-start items-center gap-0 md:gap-2 mr-4 mt-2">
-                                        <Link to={`/Dashboard/payment?cost=${item.cost}&name=${item?.name}&img=${item?.img}&choiceId=${item._id}`}><button className=" bg-[#ff9c1c] mb-4 hover:bg-[#000] text-white px-5 py-2.5 rounded-md  font-bold font-mono transition">Pay now</button></Link>
-                                        <button className=" bg-[#ff9c1c] mb-4 hover:bg-[#000] text-white px-5 py-2.5 rounded-md  font-bold font-mono transition" onClick={() => handleDelete(item._id)} >Canchel</button>
+            {
+                choice.length > 0 ?
+                    <div className=" grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4">
+                        {
+                            choice.map(item => <div key={item._id}>
+                                <div className="h-64 flex justify-center gap-2 overflow-hidden bg-base-100 shadow-xl rounded-xl mb-5">
+                                    <img className="rounded-lg w-[50%] h-64" src={item.img} alt="Album" />
+                                    <div className=" w-full flex flex-col justify-center gap-[2px] scroll-my-3">
+                                        <h2 className="text-2xl font-bold font-sans mt-2 col-span-1">{item.name}</h2>
+                                        <p className="font-bold font-mono">{item.title}</p>
+                                        <p className="font-bold font-mono col-span-1">You:{item.userName}</p>
+                                        <p className="font-bold font-mono col-span-1">Email:{item.email}</p>
+                                        <p className="font-bold font-mono">Cost:{item.cost} $</p>
+                                        <div className="flex flex-col md:flex-row justify-start items-center gap-0 md:gap-2 mr-4 mt-2">
+                                            <Link to={`/Dashboard/payment?cost=${item.cost}&name=${item?.name}&img=${item?.img}&choiceId=${item._id}`}><button className=" bg-[#ff9c1c] mb-4 hover:bg-[#000] text-white px-5 py-2.5 rounded-md  font-bold font-mono transition">Pay now</button></Link>
+                                            <button className=" bg-[#ff9c1c] mb-4 hover:bg-[#000] text-white px-5 py-2.5 rounded-md  font-bold font-mono transition" onClick={() => handleDelete(item._id)} >Canchel</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>)
-                        : <div className=" flex flex-col justify-center items-center ml-80">
-                            <h2 className="text-center text-violet-400 text-3xl font-bold font-mono mt-5">No choice added yet</h2>
-                            <h3 className=" text-center text-violet-400 text-lg my-3">Please choice your favorite place and hotel</h3>
-                            <Link to={`/allplace`}>
-                                <button className=" bg-[#ff9c1c] hover:bg-[#000] text-white px-3 py-2 rounded-md text-xl font-bold font-mono transition">Add your choice</button>
-                            </Link>
-                        </div> 
-                }
-            </div>
+                            </div>)
+                        }
+                    </div>
+                    : <div className=" flex flex-col justify-center items-center">
+                        <h2 className="text-center text-violet-400 text-3xl font-bold font-mono mt-5">No choice added yet</h2>
+                        <h3 className=" text-center text-violet-400 text-lg my-3">Please choice your favorite place and hotel</h3>
+                        <div className=" flex justify-center items-center gap-2">
+                        <Link to={`/allplace`}>
+                            <button className=" bg-[#ff9c1c] hover:bg-[#000] text-white px-3 py-2 rounded-md text-xl font-bold font-mono transition">Place</button>
+                        </Link>
+                        <Link to={'/hotel'}>
+                            <button className=" bg-[#ff9c1c] hover:bg-[#000] text-white px-3 py-2 rounded-md text-xl font-bold font-mono transition ml-2">Hotel</button>
+                        </Link>
+                        </div>
+                    </div>
+            }
             {/* <div className=" items-center flex flex-col mb-8 mt-3">
                 <Link to={`/Dashboard/payment`}>
                     <button className=" bg-[#ff9c1c] hover:bg-[#000] text-white px-6 py-4 rounded-md text-xl font-bold font-mono transition">Pay for Booking</button>
